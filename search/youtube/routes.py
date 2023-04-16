6c1c91906cb733be00e5891f16b3c2139017acca
Kai Six <petersen.codie@gmail.com>
2023-04-15 23:14:48 -0700

    Added plugin configuration files.

--- search/youtube/routes.py
+++ search/youtube/routes.py
@@ -5,11 +5,12 @@ from quart_cors import cors
 import quart
 import json
 
+BASE_ROUTE = "/search/youtube"
+CONFIG_ROUTE = "./search/youtube/config"
 app = cors(quart.Quart(__name__))
 
-@app.route("/search/youtube/transcript", methods=["POST"])
-async def search_youtube():
-    """Search youtube for the given query."""
+@app.route(f"{BASE_ROUTE}/transcript", methods=["POST"])
+async def get_video_transcript():
     data = await request.get_json()
     youtube_url = data["url"]
     transcript = get_transcript(youtube_url)
@@ -17,9 +18,41 @@ async def search_youtube():
         return json.dumps({"error": "Invalid YouTube URL."})
     return json.dumps({"transcript": transcript})
 
+# App icon route.
+@app.route(f"{BASE_ROUTE}/logo.png", methods=["GET"])
+async def plugin_logo():
+    filename = f"{CONFIG_ROUTE}/logo.png"
+    return await quart.send_file(filename, mimetype="image/png")
+
+# App manifest route.
+@app.route(f"{BASE_ROUTE}/.well-known/ai-plugin.json", methods=["GET"])
+async def plugin_manifest():
+    host = request.headers["Host"]
+    filename = f"{CONFIG_ROUTE}/ai-plugin.json"
+    with open(filename) as file:
+        text = file.read()
+        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+        return quart.Response(text, mimetype="text/json")
+
+# OpenAPI route.
+@app.route(f"{BASE_ROUTE}/openapi.yaml", methods=["GET"])
+async def openapi_specification():
+    host = request.headers["Host"]
+    filename = f"{CONFIG_ROUTE}/openapi.yaml"
+    with open(filename) as file:
+        text = file.read()
+        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+        return quart.Response(text, mimetype="text/yaml")
+
+# Health check route.
+@app.route(f"{BASE_ROUTE}/ping", methods=["GET"])
+async def ping():
+    return "pong"
+
 def youtube_service():
     """Run the YouTube Service app."""
     app.run(debug=True, host="0.0.0.0", port=5000)
+
 
 if __name__ == "__main__":
     youtube_service()
