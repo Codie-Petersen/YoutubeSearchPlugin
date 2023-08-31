72b28fe5d0fb7acdc5812e55023b688c71ce42f7
Codie Petersen <petersen.codie@gmail.com>
2023-08-30 23:12:11 -0700

    Qucik ad turn on. Will change to env style after ad test.

--- search/youtube/service.py
+++ search/youtube/service.py
@@ -108,5 +108,4 @@ async def get_promptate_ad(user_id):
     headers = {"Authorization": f"Bearer {developer_token}", "OpenAI-User-ID": user_id}
     ad = requests.get(f"https://ads.promptate.com/getad/{plugin_name}", headers=headers).json()["message"]
 
-    return None
     return ad
